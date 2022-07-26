473466f2c2d7874d58a271b143528319c47b9936
Kirill Shishkin <kirillshishkin77@gmail.com>
2022-07-27 01:50:37 +0300

    created test for extracting highlighted words

--- src/test/java/org/example/PdfExtractorTest.java
+++ src/test/java/org/example/PdfExtractorTest.java
@@ -1,8 +1,11 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.util.List;
 
 class PdfExtractorTest {
 
@@ -13,6 +16,13 @@ class PdfExtractorTest {
         // This pdf contains highlighted text
         File pdfFile = new File("src/test/resources/Willows.pdf");
         this.pdfExtractor = new PdfExtractor(pdfFile);
+    }
+
+    @Test
+    void shouldPassIfThereAreHighlightedWords() {
+        List<String> highlightedWords = this.pdfExtractor.getHighlightedWords();
+        assertNotNull(highlightedWords);
+        assertTrue(highlightedWords.size() > 0);
     }
 
 }
