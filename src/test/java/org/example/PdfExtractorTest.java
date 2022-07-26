f7f9d2e0b3a6b6d651e20aba9b7a2764c7c7424c
Kirill Shishkin <kirillshishkin77@gmail.com>
2022-07-27 02:58:48 +0300

    added one more test

--- src/test/java/org/example/PdfExtractorTest.java
+++ src/test/java/org/example/PdfExtractorTest.java
@@ -2,10 +2,12 @@ package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class PdfExtractorTest {
 
@@ -23,6 +25,13 @@ class PdfExtractorTest {
         List<String> highlightedWords = this.pdfExtractor.getHighlightedWords();
         assertNotNull(highlightedWords);
         assertTrue(highlightedWords.size() > 0);
+    }
+
+    @Test
+    void shouldPassIfReturnsAllHighlightedWords() {
+        List<String> highlightedWords = this.pdfExtractor.getHighlightedWords();
+        assertEquals(highlightedWords.size(), 6);
+        assertEquals(highlightedWords, Arrays.asList("flood", "sand", "masses of shore", "kingdom", "Donaueschingen", "Danube"));
     }
 
 }
