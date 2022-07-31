325792da618bf4f3d7e7055344b27f79210cf5bd
Kirill Shishkin <kirillshishkin77@gmail.com>
2022-07-31 22:56:15 +0300

    done TODO of extracting getting words per annotations to separate method

--- src/main/java/org/example/PdfExtractor.java
+++ src/main/java/org/example/PdfExtractor.java
@@ -41,6 +41,7 @@ public class PdfExtractor {
      * @throws WordsExtractionException
      */
     public List<String> getHighlightedWords() throws WordsExtractionException {
+
         List<String> highlightedTexts = new ArrayList<>();
 
         int pageNum = 0;
@@ -54,67 +55,72 @@ public class PdfExtractor {
                 throw new WordsExtractionException("Failed to extract words from pdf", e.getCause());
             }
 
-            //TODO: extract loop below to method: getHighlightedWordsPerPage or sth like that
-            //first setup text extraction regions
-            for (int i = 0; i < annotations.size(); i++) {
-                PDAnnotation annot = annotations.get(i);
-                var annotNote = annot.getContents(); // Conteudo anotado na nota
-                var annotSubType = annot.getSubtype(); // Tipo da nota (Highlight, Text)
-                // annotTitle = annot.getTitlePopup(); // Autor da nota
-                if (annotSubType.equals("Highlight")) {
-                    // extract highlighted text
-                    PDFTextStripperByArea stripper = null;
-                    try {
-                        stripper = new PDFTextStripperByArea();
-                    } catch (IOException e) {
-                        //TODO: consider creating specific exception sth like PdfTextStripperByAreaCreationException and throw it instead of WordsExtractionException
-                        // and then use it as cause in one global try catch
-                        throw new WordsExtractionException("Failed to extract words from pdf", e.getCause());
-                    }
-                    COSArray quadsArray = (COSArray) annot.getCOSObject().getCOSArray(COSName.getPDFName("QuadPoints"));
-                    String str = null;
-                    for (int j = 1, k = 0; j <= (quadsArray.size() / 8); j++) {
-                        //TODO: don't like how this indexes look like, it would be better to get rid of these vague indexes at all if possible
-                        Float ULX = ((COSInteger) quadsArray.get(0 + k)).floatValue();
-                        Float ULY = ((COSInteger) quadsArray.get(1 + k)).floatValue();
-                        Float URX = ((COSInteger) quadsArray.get(2 + k)).floatValue();
-                        Float URY = ((COSInteger) quadsArray.get(3 + k)).floatValue();
-                        Float LLX = ((COSInteger) quadsArray.get(4 + k)).floatValue();
-                        Float LLY = ((COSInteger) quadsArray.get(5 + k)).floatValue();
-                        Float LRX = ((COSInteger) quadsArray.get(6 + k)).floatValue();
-                        Float LRY = ((COSInteger) quadsArray.get(7 + k)).floatValue();
-                        k += 8;
-                        float ulx = ULX - 1; // upper left x.
-                        float uly = ULY; // upper left y.
-                        float width = URX - LLX;          // calculated by upperRightX - lowerLeftX.
-                        float height = URY - LLY;         // calculated by upperRightY - lowerLeftY.
-
-                        PDRectangle pageSize = pdfpage.getMediaBox();
-                        uly = pageSize.getHeight() - uly;
-
-                        Rectangle2D.Float rectangle_2 = new Rectangle2D.Float(ulx, uly, width, height);
-                        stripper.addRegion("highlightedRegion", rectangle_2);
-                        try {
-                            stripper.extractRegions(pdfpage);
-                        } catch (IOException e) {
-                            throw new WordsExtractionException("Failed to extract words from pdf", e.getCause());
-                        }
-                        String highlightedText = stripper.getTextForRegion("highlightedRegion")
-                                .replaceAll("[\\n\\t ]", " ")
-                                .replaceAll("[.!?,\\--]", "")
-                                .trim();
-
-                        if (j > 1) {
-                            str = str.concat(highlightedText);
-                        } else {
-                            str = highlightedText;
-                        }
-                    }
-                    highlightedTexts.add(str);
-                }
-            }
+            highlightedTexts.addAll(getWordsForAnnotations(pdfpage, annotations));
 
         }
         return highlightedTexts;
+    }
+
+    private List<String> getWordsForAnnotations(PDPage pdfpage, List<PDAnnotation> annotations) throws WordsExtractionException {
+        List<String> annotationTexts = new ArrayList<>();
+        for (int i = 0; i < annotations.size(); i++) {
+            PDAnnotation annot = annotations.get(i);
+            var annotNote = annot.getContents(); // Conteudo anotado na nota
+            var annotSubType = annot.getSubtype(); // Tipo da nota (Highlight, Text)
+            // annotTitle = annot.getTitlePopup(); // Autor da nota
+            if (annotSubType.equals("Highlight")) {
+                // extract highlighted text
+                PDFTextStripperByArea stripper = null;
+                try {
+                    stripper = new PDFTextStripperByArea();
+                } catch (IOException e) {
+                    //TODO: consider creating specific exception sth like PdfTextStripperByAreaCreationException and throw it instead of WordsExtractionException
+                    // and then use it as cause in one global try catch
+                    throw new WordsExtractionException("Failed to extract words from pdf", e.getCause());
+                }
+                COSArray quadsArray = (COSArray) annot.getCOSObject().getCOSArray(COSName.getPDFName("QuadPoints"));
+                String str = null;
+                for (int j = 1, k = 0; j <= (quadsArray.size() / 8); j++) {
+                    //TODO: don't like how this indexes look like, it would be better to get rid of these vague indexes at all if possible
+                    Float ULX = ((COSInteger) quadsArray.get(0 + k)).floatValue();
+                    Float ULY = ((COSInteger) quadsArray.get(1 + k)).floatValue();
+                    Float URX = ((COSInteger) quadsArray.get(2 + k)).floatValue();
+                    Float URY = ((COSInteger) quadsArray.get(3 + k)).floatValue();
+                    Float LLX = ((COSInteger) quadsArray.get(4 + k)).floatValue();
+                    Float LLY = ((COSInteger) quadsArray.get(5 + k)).floatValue();
+                    Float LRX = ((COSInteger) quadsArray.get(6 + k)).floatValue();
+                    Float LRY = ((COSInteger) quadsArray.get(7 + k)).floatValue();
+                    k += 8;
+                    float ulx = ULX - 1; // upper left x.
+                    float uly = ULY; // upper left y.
+                    float width = URX - LLX;          // calculated by upperRightX - lowerLeftX.
+                    float height = URY - LLY;         // calculated by upperRightY - lowerLeftY.
+
+                    PDRectangle pageSize = pdfpage.getMediaBox();
+                    uly = pageSize.getHeight() - uly;
+
+                    Rectangle2D.Float rectangle_2 = new Rectangle2D.Float(ulx, uly, width, height);
+                    stripper.addRegion("highlightedRegion", rectangle_2);
+                    try {
+                        stripper.extractRegions(pdfpage);
+                    } catch (IOException e) {
+                        throw new WordsExtractionException("Failed to extract words from pdf", e.getCause());
+                    }
+                    String highlightedText = stripper.getTextForRegion("highlightedRegion")
+                            .replaceAll("[\\n\\t ]", " ")
+                            .replaceAll("[.!?,\\--]", "")
+                            .trim();
+
+                    if (j > 1) {
+                        str = str.concat(highlightedText);
+                    } else {
+                        str = highlightedText;
+                    }
+                }
+                annotationTexts.add(str);
+            }
+        }
+
+        return annotationTexts;
     }
 }
