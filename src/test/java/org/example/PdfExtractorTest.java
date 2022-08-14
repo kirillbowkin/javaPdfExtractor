9490e82061309f1a788472eb9d4b2f1cc8959732
Kirill Shishkin <kirillshishkin77@gmail.com>
2022-08-14 20:49:04 +0300

    added new tests fixed previous ones

--- src/test/java/org/example/PdfExtractorTest.java
+++ src/test/java/org/example/PdfExtractorTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,6 +37,14 @@ class PdfExtractorTest {
     }
 
     @Test
+    void shouldPassIfReturnsAllHighlightedWordsForPagesRange() throws GetHighlightedWordsException {
+        List<String> highlightedWords = this.pdfExtractor.getHighlightedWords(IntStream.range(0, 1));
+        assertNotNull(highlightedWords);
+        assertEquals(4, highlightedWords.size());
+        assertEquals(highlightedWords, Arrays.asList("flood", "sand", "masses of shore", "kingdom"));
+    }
+
+    @Test
     void shouldPassIfThereAreUnderlinedWords() throws GetUnderlinedWordsException {
         List<String> underlinedWords = this.pdfExtractor.getUnderlinedWords();
         assertNotNull(underlinedWords);
@@ -45,6 +54,13 @@ class PdfExtractorTest {
     @Test
     void shouldPassIfReturnsAllUnderlinedWords() throws GetUnderlinedWordsException {
         List<String> underlinedWords = this.pdfExtractor.getUnderlinedWords();
+        assertEquals(underlinedWords.size(), 5);
+        assertEquals(underlinedWords, Arrays.asList("These willows never attain to the dignity  of  trees", "green  swells  like  the  sea", "had", "little", "craft"));
+    }
+
+    @Test
+    void shouldPassIfReturnsAllUnderlinedWordsForPagesRange() throws GetUnderlinedWordsException {
+        List<String> underlinedWords = this.pdfExtractor.getUnderlinedWords(IntStream.range(0, 1));
         assertEquals(underlinedWords.size(), 2);
         assertEquals(underlinedWords, Arrays.asList("These willows never attain to the dignity  of  trees", "green  swells  like  the  sea"));
     }
@@ -59,6 +75,13 @@ class PdfExtractorTest {
     @Test
     void shouldPassIfReturnsAllSquiggledWords() throws GetSquiggledWordsException {
         List<String> squiggledWords = this.pdfExtractor.getSquiggledWords();
+        assertEquals(squiggledWords.size(), 5);
+        assertEquals(squiggledWords, Arrays.asList("After leaving Vienna and long before you come to Budapest the Danube", "Sumpfe", "At", "heard", "many"));
+    }
+
+    @Test
+    void shouldPassIfReturnsAllSquiggledWordsForPagesRange() throws GetSquiggledWordsException {
+        List<String> squiggledWords = this.pdfExtractor.getSquiggledWords(IntStream.range(0, 1));
         assertEquals(squiggledWords.size(), 2);
         assertEquals(squiggledWords, Arrays.asList("After leaving Vienna and long before you come to Budapest the Danube", "Sumpfe"));
     }
@@ -73,6 +96,13 @@ class PdfExtractorTest {
     @Test
     void shouldPassIfReturnsAllStrokeOutWords() throws GetStrokeoutWordsException {
         List<String> strokeOutWords = this.pdfExtractor.getStrokeoutWords();
+        assertEquals(strokeOutWords.size(), 5);
+        assertEquals(strokeOutWords, Arrays.asList("becomes a swamp for miles upon miles", "marshes", "And", "pleasure", "Passau"));
+    }
+
+    @Test
+    void shouldPassIfReturnsAllStrokeOutWordsForPagesRange() throws GetStrokeoutWordsException {
+        List<String> strokeOutWords = this.pdfExtractor.getStrokeoutWords(IntStream.range(0, 1));
         assertEquals(strokeOutWords.size(), 2);
         assertEquals(strokeOutWords, Arrays.asList("becomes a swamp for miles upon miles", "marshes"));
     }
